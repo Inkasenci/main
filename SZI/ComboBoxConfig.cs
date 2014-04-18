a6daf8cdd3e34100ce05312dee4837651a586e88
Patryk <patrickus@wp.pl>
2014-04-18 12:50:56 +0200

    Komentarze

--- SZI/ComboBoxConfig.cs
+++ SZI/ComboBoxConfig.cs
@@ -34,6 +34,12 @@ namespace SZI
         /// Rekordy, ich krótkie i długie opisy.
         /// </summary>
         List<ComboBoxItem> itemList;
+        /// <summary>
+        /// Napis przechowując tekst filtrujący rekordy rozwijanej listy.
+        /// </summary>
+        /// <summary>
+        /// Podpowiedź wyświetlając aktualny tekst filtra.
+        /// </summary>
 
         /// <summary>
         /// Konwertuje tablicę napisów na jeden napis.
@@ -167,6 +173,9 @@ namespace SZI
             return this.comboBox;
         }
 
+        /// <summary>
+        /// Filtruje rekordy według aktualnej zawartości tekstu filtra. Ignoruje polskie znaki i wielkość liter.
+        /// </summary>
         private void FilterItems()
         {
             comboBox.Items.Clear();
@@ -177,6 +186,11 @@ namespace SZI
             }
         }
 
+        /// <summary>
+        /// Zamienia polskie znaki na wersje bez "ogonków".
+        /// </summary>
+        /// <param name="currentString">Napis, w którym ma zostąc dokonana zamiana.</param>
+        /// <returns>Napis po zamianie.</returns>
         private string ReplacePolishCharacters(string currentString)
         {
             string result = currentString;
@@ -194,6 +208,9 @@ namespace SZI
             return result;
         }
 
+        /// <summary>
+        /// Dodaje do rozwijanej listy wszystkie elementy przechowywane na liście elementów obiektu.
+        /// </summary>
         private void AddAllItems()
         {
             foreach (ComboBoxItem item in itemList)
@@ -203,6 +220,11 @@ namespace SZI
             }
         }
 
+        /// <summary>
+        /// Znajduje na liście elementów obiektu obiekt będący odpowiednikiem zaznaczonego na rozwijanej liście rekordu.
+        /// </summary>
+        /// <param name="item">"Aktualnie badany element listy.</param>
+        /// <returns>true - badany obiekt jest poszukiwanym obiektem.</returns>
         private bool FindItem(ComboBoxItem item)
         {
             if (item.longItemDescription == comboBox.Items[comboBox.SelectedIndex].ToString())
@@ -241,6 +263,11 @@ namespace SZI
             }
         }
 
+        /// <summary>
+        /// Wywoływana, gdy podczas przeglądania rozwijanej listy zostanie naciśnięty klawisz.
+        /// </summary>
+        /// <param name="sender">Rozwijana lista konfigurowana w ramach obiektu.</param>
+        /// <param name="e">Argumenty zdarzenia.</param>
         void comboBox_KeyDown(object sender, KeyEventArgs e)
         {
             int keyValue = e.KeyValue;
