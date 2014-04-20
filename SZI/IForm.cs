0575a00cb14bd665e896dca05fb5df05a34b1a74
unknown <rafaliusz@o2.pl>
2014-04-20 16:16:29 +0200

    IForm

---
+++ SZI/IForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZI
+{
+    /// <summary>
+    /// Interfejs który musi być implementowany przez ModifyForm i InsertForm. Potrzebny do tego, by wiedzieć czy cokolwiek w bazie zostało zmienione.
+    /// </summary>
+    interface IForm
+    {
+        /// <summary>
+        /// Zwraca wartość logiczną, która określa czy baza została zmieniona.
+        /// </summary>
+        bool Modified
+        {
+            get;
+        }
+    }
+}
